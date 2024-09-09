9607284345d1dc8da71fb453f383a07c5110bb6e
Guilherme Renato Terra De Macedo <guirenato1181@gmail.com>
2024-09-09 19:28:35 -0300

    carousel

---
+++ Tela-login/script-login.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.autoplay').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      });
+});
+
+  
